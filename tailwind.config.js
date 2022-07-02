e60ddbbb2ba31276b8175589d101b6daf92f61b5
Akash Ghosh <ag1229222@gmail.com>
2022-07-03 00:47:06 +0600

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
